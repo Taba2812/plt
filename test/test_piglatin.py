71d54125ccff8e4cf341617931e8b773b6418eff
EarlGibe <g.tabarellidefatis-1@studenti.unitn.it>
2024-10-31 16:59:39 +0200

    US1 - Test 1

--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -5,5 +5,7 @@ from error import PigLatinError
 
 class TestPigLatin(unittest.TestCase):
 
-    def test_something(self):
-        pass
+    def test_plt_get_phrase(self):
+        translator = PigLatin("hello world")
+        translator.get_phrase()
+        self.assertEqual(translator.get_phrase(), "hello world")
