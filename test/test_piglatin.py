9538493177e226cde37d4536541dacb57e805b96
EarlGibe <g.tabarellidefatis-1@studenti.unitn.it>
2024-10-31 17:25:53 +0200

    US4 - Test 1

--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -24,3 +24,18 @@ class TestPigLatin(unittest.TestCase):
         self.assertEqual(translators[0].translate(), "anynay")
         self.assertEqual(translators[1].translate(), "appleyay")
         self.assertEqual(translators[2].translate(), "askay")
+
+    def test_plt_word_starting_with_single_consonant(self):
+        translator = PigLatin("hello")
+        translator.get_phrase()
+        self.assertEqual(translator.translate(), "ellohay")
+
+# ### User Story #4 -- Translating a Word Starting with a Single Consonant
+# The input phrase can be a single word starting with a single consonant (note that the "y" letter is considered a consonant). In that case, the translator applies the following translation rule:
+# * Remove the consonant from the beginning of the word and add it to the end of the word. Finally, append “ay” to the end of the resulting word.
+#
+# **Requirement:**
+# * Implement `PigLatinTranslator.translate(self) -> str` to let the translator translate a word starting with a single consonant.
+#
+# **Example:**
+# * The translation of “hello” is “ellohay”.
